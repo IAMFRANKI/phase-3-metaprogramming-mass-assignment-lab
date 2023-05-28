5621d8262a234f781174aac5cce1287ed75ab772
frank <thmsfrank04@gmail.com>
2023-05-27 18:25:01 -0700

    Complete assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
   # your code here
-end
+
+    def initialize(attributes)
+      attributes.each do |key, value|
+        instance_variable_set("@#{key}", value)
+        self.class.send(:attr_accessor, key)
+      end
+    end
+  end
+  
